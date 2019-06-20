c1f6a5a1b8827bcf97b3bc8f73aefa4a200bdfa4
Daniel Beardsley <daniel@ifixit.com>
2019-06-20 16:13:23 -0700

    PHP Templates: add optional autescaping to echo/<?=
    Background
    ==========
    PHP doesn't have any mechanism to inject logic between templating
    and final output. There is no way to filter or alter the content
    that comes from code in templates like: <?= $someVar ?>
    
    To use php as a robust templataing language, we must inject *some*
    logic between templates and their output. We have chosen to make
    all <?=, echo, and print statements subject to an optional
    trip through escape_html_entitiles.
    
    The functionality can be toggled with `ini_set('__auto_escape')`
    and configured with `__auto_escape_flags` and
    `__auto_escape_exempt_class` (see commit
    2dae5d16436ce37856f6e00ca2a1b3009bb1f7ed for info about the class
    name based auto-escaping exemption.
    
    Methodology
    ===========
    T_ECHO (echo, <?=), T_PRINT (print) now both emit a
    ZEND_AST_ECHO_ESCAPE node in the syntax tree.
    
    That's compiled to a function which emits a ZEND_ECHO_ESCAPE op code.
    
    The op code interpretation is a dupe of ZEND_ECHO except with some
    if() statements that switch the underlying function from `zend_write`
    to `zend_write_escape` based on the ini settings.
    
    zend_write_escape is a new function pointer that points to
    php_escape_write.
    
    php_escape_write is a new function that passes it's string argument
    through php_escape_html_entities() (with __auto_escape_flags) before
    calling the underlying php_output_write.
    
    Use
    ===
    This functionality allows us to safely use php straight as a
    templating language with no template compilation step (as many
    other templating libraries have).
    
    See the included tests for more usage information.
    
    Exempt Class
    ============
    It is useful to allow some utility functions and helpers to produce
    html and have it passed straight through in the template (without
    being double-encoded). We accomplish this by *tagging* strings
    as being HTML.
    
        class HtmlString implements JsonSerializable {
           protected $html = '';
    
           public function __construct($html) {
              $this->html = $html;
           }
    
           public function __toString() {
              return (string)$this->html;
           }
    
           public function jsonSerialize() {
              return $this->html;
           }
        }
    
    The auto-escaping system can be configured with an:
    __auto_escape_exempt_class="HtmlString"
    
    Which allows instances of `HtmlString` to pass straight through a
    template without being modified (skipping the html_entities call).
    Helper functions can now return html safely and consumers don't have
    to care if it is HTML or not because the auto-escaping system knows
    what to do.
    
    squash with big commit

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -4331,6 +4331,19 @@ void zend_compile_echo(zend_ast *ast) /* {{{ */
 }
 /* }}} */
 
+void zend_compile_echo_escape(zend_ast *ast) /* {{{ */
+{
+	zend_op *opline;
+	zend_ast *expr_ast = ast->child[0];
+
+	znode expr_node;
+	zend_compile_expr(&expr_node, expr_ast);
+
+	opline = zend_emit_op(NULL, ZEND_ECHO_ESCAPE, &expr_node, NULL);
+	opline->extended_value = 0;
+}
+/* }}} */
+
 void zend_compile_throw(zend_ast *ast) /* {{{ */
 {
 	zend_ast *expr_ast = ast->child[0];
@@ -7653,7 +7666,7 @@ void zend_compile_assign_coalesce(znode *result, zend_ast *ast) /* {{{ */
 }
 /* }}} */
 
-void zend_compile_print(znode *result, zend_ast *ast) /* {{{ */
+void zend_compile_print_escape(znode *result, zend_ast *ast) /* {{{ */
 {
 	zend_op *opline;
 	zend_ast *expr_ast = ast->child[0];
@@ -7661,7 +7674,7 @@ void zend_compile_print(znode *result, zend_ast *ast) /* {{{ */
 	znode expr_node;
 	zend_compile_expr(&expr_node, expr_ast);
 
-	opline = zend_emit_op(NULL, ZEND_ECHO, &expr_node, NULL);
+	opline = zend_emit_op(NULL, ZEND_ECHO_ESCAPE, &expr_node, NULL);
 	opline->extended_value = 1;
 
 	result->op_type = IS_CONST;
@@ -8465,6 +8478,9 @@ void zend_compile_stmt(zend_ast *ast) /* {{{ */
 		case ZEND_AST_RETURN:
 			zend_compile_return(ast);
 			break;
+		case ZEND_AST_ECHO_ESCAPE:
+			zend_compile_echo_escape(ast);
+			break;
 		case ZEND_AST_ECHO:
 			zend_compile_echo(ast);
 			break;
@@ -8631,7 +8647,7 @@ void zend_compile_expr(znode *result, zend_ast *ast) /* {{{ */
 			zend_compile_assign_coalesce(result, ast);
 			return;
 		case ZEND_AST_PRINT:
-			zend_compile_print(result, ast);
+			zend_compile_print_escape(result, ast);
 			return;
 		case ZEND_AST_EXIT:
 			zend_compile_exit(result, ast);
